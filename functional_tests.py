6137861d324cd886a0f1bd7db73043ce0e1ea874
Rico <richardthomaswhitfield@gmail.com>
2017-02-18 12:58:22 -0600

    more tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,30 +18,44 @@ class NewVisitorTest(unittest.TestCase):
 		
 		#she notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		#she is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+				inputbox.get_attribute('placeholder'),
+				'Enter a to-do item'
+		)
+
+		#she types "buy some feathers" into a text box
+		inputbox.send_keys('Buy peacock feathers')
+
+		#when she hits enter the page updates and now the page lists
+		#"1: Buy some feathers" as an item on her todo list
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
 
 
 
 
-	#she types "buy some feathers" into a text box
+		#there is still another text box inviting her to add
+		#she enters something else
+		self.fail('finish the test')
+		#the page updates again and now shows both items
 
-	#when she hits enter the page updates and now the page lists
-	#"1: Buy some feathers" as an item on her todo list
+		#she wonders whether the site will remember her list
+		#then she sees that the site has generated a unique url for her
+		#there is some explanatory text to that effect
 
-	#there is still another text box inviting her to add
-	#she enters something else
+		#she visits that url her to-do list is still there
 
-	#the page updates again and now shows both items
-
-	#she wonders whether the site will remember her list
-	#then she sees that the site has generated a unique url for her
-	#there is some explanatory text to that effect
-
-	#she visits that url her to-do list is still there
-
-	#satisfied she goes back to sleep
+		#satisfied she goes back to sleep
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
